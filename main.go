a9dd83c9a03048d527c1265c1660a54583b6c27d
Florian Nehmer <florian.nehmer@posteo.de>
2024-06-17 21:23:27 +0200

    Map out functionalities

--- main.go
+++ main.go
@@ -1,8 +1,63 @@
+/**
+Plan:
+leap year: <year> % 4 == 0
+
+2024:
+___________________________________________________________
+Mo|  ▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢ |
+Tu| ▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢ |
+We| ▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢ |
+Th| ▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢  |
+Fr| ▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢  |
+Sa| ▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢  |
+Su| ▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢  |
+‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
+▢: No log
+▥: Maybe something in between
+▩: Log
+
+commands:
+- streaks show          // Display everything
+- streaks show <title>  // Display <title>
+- streaks log <title>   // Logs current date in <title>
+- streaks log <title>   // <date:YYYYMMDD> // Logs specified date in <title>
+**/
+
 package main
 
 import "fmt"
+import "time"
+
+type Weekday int
+
+const (
+	Sunday Weekday = iota
+	Monday
+	Tuesday
+	Wednesday
+	Thursday
+	Friday
+	Saturday
+)
 
 func main() {
-    fmt.Println("streaks v0")
+    var Reset string = "\033[0m"
+//    var Red string = "\033[31m" 
+//    var Green string = "\033[32m" 
+//    var Yellow string = "\033[33m" 
+    var Blue string = "\033[34m" 
+//    var Magenta string = "\033[35m" 
+//    var Cyan string = "\033[36m" 
+//    var Gray string = "\033[37m" 
+//    var White string = "\033[97m"
+
+    fmt.Println(Blue + "streaks v0" + Reset)
+
+    var somedate time.Time
+    somedate = somedate.AddDate(2023, 0, 0)
+    weekday := time.Now().Weekday()
+
+    fmt.Println(weekday)
+    fmt.Println(somedate)
 }
 
